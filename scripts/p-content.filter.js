05872e16b7e80b41fd50ff24fe3699e474ce7c3c
WesleyBlack <wesleyblackaw@gmail.com>
2020-04-06 20:09:21 -0300

    Add files via upload

--- scripts/p-content.filter.js
+++ scripts/p-content.filter.js
@@ -24,39 +24,61 @@ function hashChanged(hash) {
         if (webDesign.classList.contains('show')) {
             webDesign.classList.remove('show');
             webDesign.classList.add('hide');
+            setTimeout(function() {
+                webDesign.style.display = 'none';
+            }, 500);
         } else if (andMore.classList.contains('show')){
             andMore.classList.remove('show');
             andMore.classList.add('hide');
+            setTimeout(function() {
+                andMore.style.display = 'none';
+            }, 500);
         };
         if (identities.classList.contains('hide')) {
-                identities.classList.remove('hide');
-                identities.classList.add('show');
+            identities.classList.remove('hide');
+            identities.classList.add('show');
+            identities.style.display = 'inline-grid';
         };
-        } else if (hash == '#web-design') {
-            if (identities.classList.contains('show')) {
-                identities.classList.remove('show');
-                identities.classList.add('hide');
-            } else if (andMore.classList.contains('show')){
-                andMore.classList.remove('show');
-                andMore.classList.add('hide');
-            };
-            if (webDesign.classList.contains('hide')) {
-                webDesign.classList.remove('hide');
-                webDesign.classList.add('show');
-            };
-        } else if (hash == '#other') {
-            if (identities.classList.contains('show')) {
-                identities.classList.remove('show');
-                identities.classList.add('hide');
-            } else if (webDesign.classList.contains('show')){
-                webDesign.classList.remove('show');
-                webDesign.classList.add('hide');
-            };
-            if (andMore.classList.contains('hide')) {
-                andMore.classList.remove('hide');
-                andMore.classList.add('show');
-            };
-        }
+        
+    } else if (hash == '#web-design') {
+        if (identities.classList.contains('show')) {
+            identities.classList.remove('show');
+            identities.classList.add('hide');
+            setTimeout(function() {
+                identities.style.display = 'none';
+            }, 500);
+        } else if (andMore.classList.contains('show')){
+            andMore.classList.remove('show');
+            andMore.classList.add('hide');
+            setTimeout(function() {
+                andMore.style.display = 'none';
+            }, 500);
+        };
+        if (webDesign.classList.contains('hide')) {
+            webDesign.classList.remove('hide');
+            webDesign.classList.add('show');
+            webDesign.style.display = 'inline-grid';
+        };
+    } else if (hash == '#other') {
+        if (identities.classList.contains('show')) {
+            identities.classList.remove('show');
+            identities.classList.add('hide');
+            setTimeout(function() {
+                identities.style.display = 'none';
+            }, 500);
+        } else if (webDesign.classList.contains('show')){
+            webDesign.classList.remove('show');
+            webDesign.classList.add('hide');
+            setTimeout(function() {
+                webDesign.style.display = 'none';
+            }, 500);
+        };
+        if (andMore.classList.contains('hide')) {
+            andMore.classList.remove('hide');
+            andMore.classList.add('show');
+            andMore.style.display = 'inline-grid';
+        };
+    }
 };
      
 categoriesChanged(window.location.hash);
